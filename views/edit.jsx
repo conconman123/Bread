df951904956c15f401d30c90bceb48a9ec81503e
Conor Panuccio <conorpanu@hotmail.com>
2023-07-17 20:07:46 -0400

    started database inclusion

--- views/edit.jsx
+++ views/edit.jsx
@@ -1,43 +1,38 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function New () {
+function Edit({bread, index}) {
     return (
       <Default>
-        <h2>Add a new bread</h2>
-        <form action = '/breads' method='POST'>
+        <h2>Edit a bread</h2>
+        <form action={`/breads/${index}?_method=PUT`} method='POST'>
           <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
             id="name"
             required
+            defaultValue={bread.name}
           />
           <label htmlFor="image">Image</label>
           <input
             type="text"
             name="image"
-            id="image"/>
+            id="image"
+            defaultValue={bread.image}
+          />
           <label htmlFor="hasGluten">Has Gluten?</label>
           <input
             type="checkbox"
             name="hasGluten"
             id="hasGluten"
-            defaultChecked
+            defaultChecked={bread.hasGluten}
           />
           <br />
           <input type="submit"/>
         </form>
-
-        <div>
-            <a href = '/breads'>
-            <button>
-                Back
-            </button>
-            </a>
-        </div>
       </Default>
     )
 }
 
-module.exports = New
+module.exports = Edit
--- views/show.jsx
+++ views/show.jsx
@@ -13,6 +13,9 @@ function Show ({bread, index}) {
           }
           have glutten!
         </p>
+        <a href={`/breads/${index}/edit`}>
+          <button>Edit</button>
+        </a>
         <img src={bread.image} alt={bread.name} />
         <form action={`/breads/${index}?_method=DELETE`} method='POST'>
           <input type="submit" value='DELETE' />
